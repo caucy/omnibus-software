fc848a3f2a4c0c158549c756c461be1be3ab0629
kexibuaijifen <dmwan_0926@126.com>
2017-03-11 12:45:49 +0800

    Update zlib.rb

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -16,10 +16,10 @@
 #
 
 name "zlib"
-default_version "1.2.8"
+default_version "1.2.11"
 
-version "1.2.6" do
-  source md5: "618e944d7c7cd6521551e30b32322f4a"
+version "1.2.11" do
+  source sha256: "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
 end
 
 version "1.2.8" do
